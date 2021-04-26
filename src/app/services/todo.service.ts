4e1e4077af13879d9ad043b3064f19b3e1b7d603
imkarin <karinmeijvogel@hotmail.com>
2021-04-26 17:11:42 +0200

    Fetching to-dos from jsonplaceholder API in TodoService

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl = 'https://jsonplaceholder.typicode.com/todos';
+  query = '?_limit=5&completed=false'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Do groceries',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Watch movie',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Work on project',
-        completed: false
-      }
-    ]
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.query}`);
   }
 }
